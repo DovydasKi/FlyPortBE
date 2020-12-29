ddbcba412fdb8c91ad52b0f127a01424cf22f268
DovydasKi <dovydasdk@gmail.com>
2020-12-29 17:14:49 +0200

    Initial commit

---
+++ src/main/java/com/example/flyport/FlyportApplication.java
@@ -0,0 +1,13 @@
+package com.example.flyport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlyportApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlyportApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/flyport/FlyportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.flyport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlyportApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
