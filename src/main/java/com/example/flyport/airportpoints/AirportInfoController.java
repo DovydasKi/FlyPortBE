e4ffb44d69937e29e0f4beba0ef9b3e949382e0e
DovydasKi <dovydasdk@gmail.com>
2021-01-26 19:46:00 +0200

    Last endpoints

--- src/main/java/com/example/flyport/airportpoints/AirportInfoController.java
+++ src/main/java/com/example/flyport/airportpoints/AirportInfoController.java
@@ -21,7 +21,7 @@ public class AirportInfoController {
     private String registerUserFlight(@RequestBody UserFlightCreationModel userFlightCreationModel) throws SQLException, ClassNotFoundException {
         jdbc.beginDBSession();
 
-        String query = airportPointQueries.registerUserFlight;
+        String query = airportPointQueries.registrationPoint.registerUserFlight;
         PreparedStatement preparedStatement = jdbc.connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
         preparedStatement.setInt(1, userFlightCreationModel.getUserId());
         preparedStatement.setInt(2, userFlightCreationModel.getFlightId());
@@ -41,19 +41,19 @@ public class AirportInfoController {
     private String findRegistrationAirportPoint(@PathVariable int userFlightId) throws SQLException, ClassNotFoundException {
         jdbc.beginDBSession();
 
-        String query = airportPointQueries.addRegistrationPointToUserFlight;
+        String query = airportPointQueries.registrationPoint.addRegistrationPointToUserFlight;
         PreparedStatement preparedStatement = jdbc.connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
         preparedStatement.setInt(1, userFlightId);
         preparedStatement.executeUpdate();
         preparedStatement.close();
 
-        String queryForUpdatingQueue = airportPointQueries.addQueueToRegistrationPoint;
+        String queryForUpdatingQueue = airportPointQueries.registrationPoint.addQueueToRegistrationPoint;
         PreparedStatement queueUpdatingStatement = jdbc.connection.prepareStatement(queryForUpdatingQueue);
         queueUpdatingStatement.setInt(1, userFlightId);
         queueUpdatingStatement.executeUpdate();
         queueUpdatingStatement.close();
 
-        String queryForRegistrationPoint = airportPointQueries.getRegistrationPoint;
+        String queryForRegistrationPoint = airportPointQueries.registrationPoint.getRegistrationPoint;
         PreparedStatement pointStatement = jdbc.connection.prepareStatement(queryForRegistrationPoint);
         pointStatement.setInt(1, userFlightId);
         ResultSet resultSet = pointStatement.executeQuery();
@@ -72,7 +72,142 @@ public class AirportInfoController {
     private void leaveRegistrationPoint(@PathVariable int userFlightId) throws SQLException, ClassNotFoundException {
         jdbc.beginDBSession();
 
-        String query = airportPointQueries.removeQueueFromRegistrationPoint;
+        String query = airportPointQueries.registrationPoint.removeQueueFromRegistrationPoint;
+        PreparedStatement preparedStatement = jdbc.connection.prepareStatement(query);
+        preparedStatement.setInt(1, userFlightId);
+        int success = preparedStatement.executeUpdate();
+        if (success <= 0) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Internal server error");
+        } else {
+            throw new ResponseStatusException(HttpStatus.OK, "OK");
+        }
+
+    }
+
+    @GetMapping("user/flight/{userFlightId}/point/security")
+    private String findSecurityAirportPoint(@PathVariable int userFlightId) throws SQLException, ClassNotFoundException {
+        jdbc.beginDBSession();
+
+        String query = airportPointQueries.securityPoint.addSecurityPointToUserFlight;
+        PreparedStatement preparedStatement = jdbc.connection.prepareStatement(query);
+        preparedStatement.setInt(1, userFlightId);
+        preparedStatement.executeUpdate();
+        preparedStatement.close();
+
+        String queryForUpdatingQueue = airportPointQueries.securityPoint.addQueueToSecurityPoint;
+        PreparedStatement queueUpdatingStatement = jdbc.connection.prepareStatement(queryForUpdatingQueue);
+        queueUpdatingStatement.setInt(1, userFlightId);
+        queueUpdatingStatement.executeUpdate();
+        queueUpdatingStatement.close();
+
+        String queryForSecurityPoint = airportPointQueries.securityPoint.getSecurityPoint;
+        PreparedStatement pointStatement = jdbc.connection.prepareStatement(queryForSecurityPoint);
+        pointStatement.setInt(1, userFlightId);
+        ResultSet resultSet = pointStatement.executeQuery();
+        if (resultSet.next()) {
+            int pointId = resultSet.getInt(1);
+            String type = resultSet.getString(2);
+            String number = resultSet.getString(3);
+            AirportPointModel airportPointModel = new AirportPointModel(pointId, type, number);
+            return parser.toJson(airportPointModel);
+        } else {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Problem with data base");
+        }
+    }
+
+    @PutMapping("user/flight/{userFlightId}/point/security")
+    private void leaveSecurityPoint(@PathVariable int userFlightId) throws SQLException, ClassNotFoundException {
+        jdbc.beginDBSession();
+
+        String query = airportPointQueries.securityPoint.removeQueueFromSecurityPoint;
+        PreparedStatement preparedStatement = jdbc.connection.prepareStatement(query);
+        preparedStatement.setInt(1, userFlightId);
+        int success = preparedStatement.executeUpdate();
+        if (success <= 0) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Internal server error");
+        } else {
+            throw new ResponseStatusException(HttpStatus.OK, "OK");
+        }
+
+    }
+
+    @GetMapping("user/flight/{userFlightId}/point/passport")
+    private String findPassportAirportPoint(@PathVariable int userFlightId) throws SQLException, ClassNotFoundException {
+        jdbc.beginDBSession();
+
+        String query = airportPointQueries.passportPoint.addPassportPointToUserFlight;
+        PreparedStatement preparedStatement = jdbc.connection.prepareStatement(query);
+        preparedStatement.setInt(1, userFlightId);
+        preparedStatement.executeUpdate();
+        preparedStatement.close();
+
+        String queryForUpdatingQueue = airportPointQueries.passportPoint.addQueueToPassportPoint;
+        PreparedStatement queueUpdatingStatement = jdbc.connection.prepareStatement(queryForUpdatingQueue);
+        queueUpdatingStatement.setInt(1, userFlightId);
+        queueUpdatingStatement.executeUpdate();
+        queueUpdatingStatement.close();
+
+        String queryForSecurityPoint = airportPointQueries.passportPoint.getPassportPoint;
+        PreparedStatement pointStatement = jdbc.connection.prepareStatement(queryForSecurityPoint);
+        pointStatement.setInt(1, userFlightId);
+        ResultSet resultSet = pointStatement.executeQuery();
+        if (resultSet.next()) {
+            int pointId = resultSet.getInt(1);
+            String type = resultSet.getString(2);
+            String number = resultSet.getString(3);
+            AirportPointModel airportPointModel = new AirportPointModel(pointId, type, number);
+            return parser.toJson(airportPointModel);
+        } else {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Problem with data base");
+        }
+    }
+
+    @PutMapping("user/flight/{userFlightId}/point/passport")
+    private void leavePassportPoint(@PathVariable int userFlightId) throws SQLException, ClassNotFoundException {
+        jdbc.beginDBSession();
+
+        String query = airportPointQueries.passportPoint.removeQueueFromPassportPoint;
+        PreparedStatement preparedStatement = jdbc.connection.prepareStatement(query);
+        preparedStatement.setInt(1, userFlightId);
+        int success = preparedStatement.executeUpdate();
+        if (success <= 0) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Internal server error");
+        } else {
+            throw new ResponseStatusException(HttpStatus.OK, "OK");
+        }
+
+    }
+
+    @GetMapping("user/flight/{userFlightId}/point/boarding")
+    private String findBoardingAirportGates(@PathVariable int userFlightId) throws SQLException, ClassNotFoundException {
+        jdbc.beginDBSession();
+
+        String query = airportPointQueries.boardingGates.addBoardingGatesToUserFlight;
+        PreparedStatement preparedStatement = jdbc.connection.prepareStatement(query);
+        preparedStatement.setInt(1, userFlightId);
+        preparedStatement.executeUpdate();
+        preparedStatement.close();
+
+        String queryForSecurityPoint = airportPointQueries.boardingGates.getBoardingGates;
+        PreparedStatement pointStatement = jdbc.connection.prepareStatement(queryForSecurityPoint);
+        pointStatement.setInt(1, userFlightId);
+        ResultSet resultSet = pointStatement.executeQuery();
+        if (resultSet.next()) {
+            int pointId = resultSet.getInt(1);
+            String type = resultSet.getString(2);
+            String number = resultSet.getString(3);
+            AirportPointModel airportPointModel = new AirportPointModel(pointId, type, number);
+            return parser.toJson(airportPointModel);
+        } else {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Problem with data base");
+        }
+    }
+
+    @PutMapping("user/flight/{userFlightId}/complete")
+    private void completeFlight(@PathVariable int userFlightId) throws SQLException, ClassNotFoundException {
+        jdbc.beginDBSession();
+
+        String query = airportPointQueries.boardingGates.completeFlight;
         PreparedStatement preparedStatement = jdbc.connection.prepareStatement(query);
         preparedStatement.setInt(1, userFlightId);
         int success = preparedStatement.executeUpdate();
--- src/main/java/com/example/flyport/airportpoints/AirportPointQueries.java
+++ src/main/java/com/example/flyport/airportpoints/AirportPointQueries.java
@@ -1,47 +1,165 @@
 package com.example.flyport.airportpoints;
 
 public class AirportPointQueries {
-    public final String registerUserFlight = "INSERT INTO USER_FLIGHT (USER_ID, FLIGHT_ID) VALUES (?,?)";
-    public final String addRegistrationPointToUserFlight = "UPDATE " +
-            "    USER_FLIGHT A " +
-            "SET " +
-            "    A.REGISTRATION_POINT = ( " +
-            "        SELECT " +
-            "            AIRPORT_POINT_ID " +
-            "        FROM " +
-            "            AIRPORT_POINT " +
-            "        WHERE " +
-            "                TYPE = 'R' " +
-            "        ORDER BY QUEUE ASC " +
-            "        LIMIT 1 " +
-            "    ) " +
-            "WHERE " +
-            "        A.USER_FLIGHT_ID = ?";
-    public final String addQueueToRegistrationPoint = "UPDATE " +
-            "    AIRPORT_POINT A " +
-            "SET " +
-            "    A.QUEUE = A.QUEUE + 1 " +
-            "WHERE " +
-            "        AIRPORT_POINT_ID = ( " +
-            "        SELECT " +
-            "            REGISTRATION_POINT " +
-            "        FROM " +
-            "            USER_FLIGHT " +
-            "        WHERE " +
-            "                USER_FLIGHT_ID = ? " +
-            "    )";
-    public final String getRegistrationPoint = "SELECT A.AIRPORT_POINT_ID, A.TYPE, A.POINT_NUMBER FROM AIRPORT_POINT A, USER_FLIGHT B WHERE B.USER_FLIGHT_ID=? AND A.AIRPORT_POINT_ID = B.REGISTRATION_POINT";
-    public final String removeQueueFromRegistrationPoint = "UPDATE " +
-            "    AIRPORT_POINT A " +
-            "SET " +
-            "    A.QUEUE = A.QUEUE - 1 " +
-            "WHERE " +
-            "        AIRPORT_POINT_ID = ( " +
-            "        SELECT " +
-            "            REGISTRATION_POINT " +
-            "        FROM " +
-            "            USER_FLIGHT " +
-            "        WHERE " +
-            "                USER_FLIGHT_ID = ? " +
-            "    )";
+    public RegistrationPoint registrationPoint;
+    public BoardingGates boardingGates;
+    public SecurityPoint securityPoint;
+    public PassportPoint passportPoint;
+
+    AirportPointQueries() {
+        this.registrationPoint = new RegistrationPoint();
+        this.boardingGates = new BoardingGates();
+        this.securityPoint = new SecurityPoint();
+        this.passportPoint = new PassportPoint();
+    }
+
+    public final class RegistrationPoint {
+        public final String registerUserFlight = "INSERT INTO USER_FLIGHT (USER_ID, FLIGHT_ID) VALUES (?,?)";
+        public final String addRegistrationPointToUserFlight = "UPDATE " +
+                "    USER_FLIGHT A " +
+                "SET " +
+                "    A.REGISTRATION_POINT = ( " +
+                "        SELECT " +
+                "            AIRPORT_POINT_ID " +
+                "        FROM " +
+                "            AIRPORT_POINT " +
+                "        WHERE " +
+                "                TYPE = 'R' " +
+                "        ORDER BY QUEUE ASC " +
+                "        LIMIT 1 " +
+                "    ) " +
+                "WHERE " +
+                "        A.USER_FLIGHT_ID = ?";
+        public final String addQueueToRegistrationPoint = "UPDATE " +
+                "    AIRPORT_POINT A " +
+                "SET " +
+                "    A.QUEUE = A.QUEUE + 1 " +
+                "WHERE " +
+                "        AIRPORT_POINT_ID = ( " +
+                "        SELECT " +
+                "            REGISTRATION_POINT " +
+                "        FROM " +
+                "            USER_FLIGHT " +
+                "        WHERE " +
+                "                USER_FLIGHT_ID = ? " +
+                "    )";
+        public final String getRegistrationPoint = "SELECT A.AIRPORT_POINT_ID, A.TYPE, A.POINT_NUMBER FROM AIRPORT_POINT A, USER_FLIGHT B WHERE B.USER_FLIGHT_ID=? AND A.AIRPORT_POINT_ID = B.REGISTRATION_POINT";
+        public final String removeQueueFromRegistrationPoint = "UPDATE " +
+                "    AIRPORT_POINT A " +
+                "SET " +
+                "    A.QUEUE = A.QUEUE - 1 " +
+                "WHERE " +
+                "        AIRPORT_POINT_ID = ( " +
+                "        SELECT " +
+                "            REGISTRATION_POINT " +
+                "        FROM " +
+                "            USER_FLIGHT " +
+                "        WHERE " +
+                "                USER_FLIGHT_ID = ? " +
+                "    )";
+    };
+
+    public final class SecurityPoint {
+        public final String addSecurityPointToUserFlight = "UPDATE " +
+                "    USER_FLIGHT A " +
+                "SET " +
+                "    A.SECURITY_POINT = ( " +
+                "        SELECT " +
+                "            AIRPORT_POINT_ID " +
+                "        FROM " +
+                "            AIRPORT_POINT " +
+                "        WHERE " +
+                "                TYPE = 'S' " +
+                "        ORDER BY QUEUE ASC " +
+                "        LIMIT 1 " +
+                "    ) " +
+                "WHERE " +
+                "        A.USER_FLIGHT_ID = ?";
+        public final String addQueueToSecurityPoint = "UPDATE " +
+                "    AIRPORT_POINT A " +
+                "SET " +
+                "    A.QUEUE = A.QUEUE + 1 " +
+                "WHERE " +
+                "        AIRPORT_POINT_ID = ( " +
+                "        SELECT " +
+                "            SECURITY_POINT " +
+                "        FROM " +
+                "            USER_FLIGHT " +
+                "        WHERE " +
+                "                USER_FLIGHT_ID = ? " +
+                "    )";
+        public final String getSecurityPoint = "SELECT A.AIRPORT_POINT_ID, A.TYPE, A.POINT_NUMBER FROM AIRPORT_POINT A, USER_FLIGHT B WHERE B.USER_FLIGHT_ID=? AND A.AIRPORT_POINT_ID = B.SECURITY_POINT";
+        public final String removeQueueFromSecurityPoint = "UPDATE " +
+                "    AIRPORT_POINT A " +
+                "SET " +
+                "    A.QUEUE = A.QUEUE - 1 " +
+                "WHERE " +
+                "        AIRPORT_POINT_ID = ( " +
+                "        SELECT " +
+                "            SECURITY_POINT " +
+                "        FROM " +
+                "            USER_FLIGHT " +
+                "        WHERE " +
+                "                USER_FLIGHT_ID = ? " +
+                "    )";
+    };
+
+    public final class BoardingGates {
+        public final String addBoardingGatesToUserFlight = "UPDATE " +
+                "  USER_FLIGHT A " +
+                "  INNER JOIN FLIGHT B " +
+                "SET " +
+                "  A.BOARDING_GATES = B.GATE " +
+                "WHERE " +
+                "  A.USER_FLIGHT_ID = ? " +
+                "  AND A.FLIGHT_ID = B.FLIGHT_ID";
+        public final String getBoardingGates = "SELECT A.AIRPORT_POINT_ID, A.TYPE, A.POINT_NUMBER FROM AIRPORT_POINT A, USER_FLIGHT B WHERE B.USER_FLIGHT_ID=? AND A.AIRPORT_POINT_ID = B.BOARDING_GATES";
+        public final String completeFlight = "UPDATE USER_FLIGHT A SET COMPLETED=1 WHERE A.USER_FLIGHT_ID = ?";
+    };
+
+    public final class PassportPoint {
+        public final String addPassportPointToUserFlight = "UPDATE " +
+                "    USER_FLIGHT A " +
+                "SET " +
+                "    A.PASSPORT_POINT = ( " +
+                "        SELECT " +
+                "            AIRPORT_POINT_ID " +
+                "        FROM " +
+                "            AIRPORT_POINT " +
+                "        WHERE " +
+                "                TYPE = 'P' " +
+                "        ORDER BY QUEUE ASC " +
+                "        LIMIT 1 " +
+                "    ) " +
+                "WHERE " +
+                "        A.USER_FLIGHT_ID = ?";
+        public final String addQueueToPassportPoint = "UPDATE " +
+                "    AIRPORT_POINT A " +
+                "SET " +
+                "    A.QUEUE = A.QUEUE + 1 " +
+                "WHERE " +
+                "        AIRPORT_POINT_ID = ( " +
+                "        SELECT " +
+                "            PASSPORT_POINT " +
+                "        FROM " +
+                "            USER_FLIGHT " +
+                "        WHERE " +
+                "                USER_FLIGHT_ID = ? " +
+                "    )";
+        public final String getPassportPoint = "SELECT A.AIRPORT_POINT_ID, A.TYPE, A.POINT_NUMBER FROM AIRPORT_POINT A, USER_FLIGHT B WHERE B.USER_FLIGHT_ID=? AND A.AIRPORT_POINT_ID = B.PASSPORT_POINT";
+        public final String removeQueueFromPassportPoint = "UPDATE " +
+                "    AIRPORT_POINT A " +
+                "SET " +
+                "    A.QUEUE = A.QUEUE - 1 " +
+                "WHERE " +
+                "        AIRPORT_POINT_ID = ( " +
+                "        SELECT " +
+                "            PASSPORT_POINT " +
+                "        FROM " +
+                "            USER_FLIGHT " +
+                "        WHERE " +
+                "                USER_FLIGHT_ID = ? " +
+                "    )";
+    };
+
 }
