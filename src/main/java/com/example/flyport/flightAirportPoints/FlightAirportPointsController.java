fc491fafce69eadab7c40f402b4b628c1901ff50
DovydasKi <dovydasdk@gmail.com>
2021-03-09 21:26:56 +0200

    Bugfix for flight airport points crash

--- src/main/java/com/example/flyport/flightAirportPoints/FlightAirportPointsController.java
+++ src/main/java/com/example/flyport/flightAirportPoints/FlightAirportPointsController.java
@@ -2,11 +2,9 @@ package com.example.flyport.flightAirportPoints;
 
 import com.example.flyport.database.JDBCLogin;
 import com.google.gson.Gson;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -15,40 +13,51 @@ import java.sql.SQLException;
 @RestController
 public class FlightAirportPointsController {
 
-    private JDBCLogin jdbc = new JDBCLogin();
-    private Gson parser = new Gson();
+    private final JDBCLogin jdbc = new JDBCLogin();
+    private final Gson parser = new Gson();
 
     @GetMapping("flights/airportPoints/{flightId}")
     private String getUserFlights(@PathVariable int flightId) throws SQLException, ClassNotFoundException {
         jdbc.beginDBSession();
 
-        String query = "SELECT * FROM AIRPORT_POINT A, AIRPORT_POINT B, AIRPORT_POINT C, AIRPORT_POINT D, USER_FLIGHT E " +
-                "WHERE E.USER_FLIGHT_ID = ? " +
-                "AND E.REGISTRATION_POINT=A.AIRPORT_POINT_ID " +
-                "AND E.SECURITY_POINT=B.AIRPORT_POINT_ID " +
-                "AND E.BOARDING_GATES=C.AIRPORT_POINT_ID " +
-                "AND E.PASSPORT_POINT=D.AIRPORT_POINT_ID ";
+        String queryForRegistrationTable = "SELECT * FROM AIRPORT_POINT A, USER_FLIGHT E WHERE E.USER_FLIGHT_ID = ? AND E.REGISTRATION_POINT=A.AIRPORT_POINT_ID";
+        String queryForAviationSecurityTable = "SELECT * FROM AIRPORT_POINT A, USER_FLIGHT E WHERE E.USER_FLIGHT_ID = ? AND E.SECURITY_POINT=A.AIRPORT_POINT_ID";
+        String queryForPassportTable = "SELECT * FROM AIRPORT_POINT A, USER_FLIGHT E WHERE E.USER_FLIGHT_ID = ? AND E.PASSPORT_POINT=A.AIRPORT_POINT_ID";
+        String queryForBoardingGates = "SELECT * FROM AIRPORT_POINT A, USER_FLIGHT E WHERE E.USER_FLIGHT_ID = ? AND E.BOARDING_GATES=A.AIRPORT_POINT_ID";
 
-        PreparedStatement preparedStatement = jdbc.connection.prepareStatement(query);
-        preparedStatement.setInt(1, flightId);
-        ResultSet resultSet = preparedStatement.executeQuery();
+        PreparedStatement registrationPreparedStatement = jdbc.connection.prepareStatement(queryForRegistrationTable);
+        PreparedStatement securityPreparedStatement = jdbc.connection.prepareStatement(queryForAviationSecurityTable);
+        PreparedStatement passportPreparedStatement = jdbc.connection.prepareStatement(queryForPassportTable);
+        PreparedStatement boardingPreparedStatement = jdbc.connection.prepareStatement(queryForBoardingGates);
 
-        FlightAirportPointsModel flightAirportPointsModel = this.convertToAirportPoints(resultSet);
+        registrationPreparedStatement.setInt(1, flightId);
+        securityPreparedStatement.setInt(1, flightId);
+        passportPreparedStatement.setInt(1, flightId);
+        boardingPreparedStatement.setInt(1, flightId);
+
+        ResultSet registrationResultSet = registrationPreparedStatement.executeQuery();
+        ResultSet securityResultSet = securityPreparedStatement.executeQuery();
+        ResultSet passportResultSet = passportPreparedStatement.executeQuery();
+        ResultSet boardingResultSet = boardingPreparedStatement.executeQuery();
+
+        FlightAirportPointsModel flightAirportPointsModel = this.convertToAirportPoints(registrationResultSet, securityResultSet, passportResultSet, boardingResultSet);
         return parser.toJson(flightAirportPointsModel);
     }
 
-        private FlightAirportPointsModel convertToAirportPoints(ResultSet resultSet) throws SQLException {
+    private FlightAirportPointsModel convertToAirportPoints(ResultSet regResultSet, ResultSet secResultSet, ResultSet passResultSet, ResultSet boardResultSet) throws SQLException {
+        FlightAirportPointModel registrationPoint = this.getModel(regResultSet);
+        FlightAirportPointModel securityPoint = this.getModel(secResultSet);
+        FlightAirportPointModel boardingGates = this.getModel(boardResultSet);
+        FlightAirportPointModel passportPoint = this.getModel(passResultSet);
 
-            if(resultSet.next()) {
-                FlightAirportPointModel registrationPoint = new FlightAirportPointModel(resultSet.getString(3), resultSet.getString(2));
-                FlightAirportPointModel securityPoint = new FlightAirportPointModel(resultSet.getString(7), resultSet.getString(6));
-                FlightAirportPointModel boardingGates = new FlightAirportPointModel(resultSet.getString(11), resultSet.getString(10));
-                FlightAirportPointModel passportPoint = new FlightAirportPointModel(resultSet.getString(15), resultSet.getString(14));
+        return new FlightAirportPointsModel(registrationPoint, securityPoint, passportPoint, boardingGates);
+    }
 
-                FlightAirportPointsModel flightAirportPointsModel = new FlightAirportPointsModel(registrationPoint, securityPoint, passportPoint, boardingGates);
-                return flightAirportPointsModel;
-            } else {
-                throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Not found");
-            }
+    private FlightAirportPointModel getModel(ResultSet resultSet) throws SQLException {
+        if (resultSet.next()) {
+             return new FlightAirportPointModel(resultSet.getString(3), resultSet.getString(2));
+        } else {
+            return null;
         }
     }
+}
